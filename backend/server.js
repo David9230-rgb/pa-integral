fd8fdfdbf2db9a9073bb106df370595c5dc64013
Tu Nombre <tu_email@ejemplo.com>
2026-07-22 15:50:12 -0500

    Ajustes de server y CORS

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ app.use("/api/pedidos", pedidoRoutes);
 const server = http.createServer(app);
 
 
-const io = new Server(server,{
-  cors:{
-    origin:"http://localhost:5173",
-    methods:["GET","POST"]
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
   }
 });
 
